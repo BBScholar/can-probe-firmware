a9ef40d25823240350706a8bb4ee6686e9d4f1e6
Ben Scholar <bbs27@case.edu>
2021-01-31 20:21:15 -0500

    git rid of std lib issue

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 #![no_std]
 #![allow(dead_code, unused_imports)]
+#![no_main]
 
-use adaptor_core;
+use core::prelude::*;
+use embedded_hal::prelude::*;
+use stm32f1xx_hal::prelude::*;
 
-use adaptor_core::AdaptorSettings;
+use defmt;
+use defmt_rtt as _;
 
-fn main() {}
+use rtic::app;
+use rtic::cyccnt::{Instant, U32Ext as _};
+
+#[defmt::timestamp]
+fn timestamp() -> u64 {
+    cortex_m::peripheral::DWT::get_cycle_count() as u64
+}
+
+#[app(device=stm32f1::stm32f103, peripherals=true, monotonic=rtic::cyccnt::CYCCNT)]
+const APP: () = {};
